85c5bfe361756dc9bb35f97fab3e5574db8746ec
Cynthia Costa <cyxcosta@gmail.com>
2020-10-29 08:12:52 -0300

    Small adjustments

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -8,13 +8,13 @@ function Dropdown({ cities, selectedCity, setSelectedCity }) {
     <div className="dropdown">
       <label onClick={() => setDropdownOpen(!dropdownOpen)} className="dropdown__options-filter">
         <ul className={`dropdown__filter ${dropdownOpen ? 'dropdown__filter--open' : ''}`} role="listbox" tabIndex="-1">
-          <li className="dropdown__filter-selected" aria-selected="true">
+          <li className="dropdown__filter-selected">
             {selectedCity.name || 'Choose your city'}
           </li>
           <li>
             <ul className={`dropdown__select ${dropdownOpen ? 'dropdown__select--open' : ''}`}>
               {cities.map((city) => (
-                <li key={city.id} onClick={() => setSelectedCity(city)} className="dropdown__select-option" role="option">
+                <li key={city.id} onClick={() => setSelectedCity(city)} className="dropdown__select-option">
                   {city.name}
                 </li>
               ))}
