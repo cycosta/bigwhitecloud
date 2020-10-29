6bdda14fb7b557279f7478fe3f42b6b3ff48ecee
Cynthia Costa <cyxcosta@gmail.com>
2020-10-29 16:29:14 -0300

    Close dropdown on outside click

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 
 function Dropdown({ cities, selectedCity, setSelectedCity }) {
   const [dropdownOpen, setDropdownOpen] = useState(false)
 
+  const dropdownElement = useRef()
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+  }, [])
+
+  const handleClick = e => {
+    if (dropdownElement.current.contains(e.target)) {
+      return;
+    }
+    setDropdownOpen(false)
+  };
+
   return (
-    <div className="dropdown">
+    <div className="dropdown" ref={dropdownElement}>
       <label onClick={() => setDropdownOpen(!dropdownOpen)} className="dropdown__options-filter">
         <ul className={`dropdown__filter ${dropdownOpen ? 'dropdown__filter--open' : ''}`} role="listbox" tabIndex="-1">
           <li className="dropdown__filter-selected">
