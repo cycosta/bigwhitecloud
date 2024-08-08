4ef6fb6a8bf513b3baec69510be415cc53303546
cynthia.costa <cynthia.costa@quintoandar.com.br>
2024-08-07 21:59:07 -0300

    fix: remove unused logo

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,5 @@ export { Card } from "./Card";
 export { Copyright } from "./Copyright";
 export { Dropdown } from "./Dropdown";
 export { Loader } from "./Loader";
-export { Logo } from "./Logo";
 export { Weather } from "./Weather";
 export { Wrapper } from "./Wrapper";
